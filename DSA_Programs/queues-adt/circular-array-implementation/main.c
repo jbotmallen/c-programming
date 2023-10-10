ff6c263a2533d989dbe9b5c3afd37ed77e1cc9e5
Chuyaks123 <66161070+shetbricks21@users.noreply.github.com>
2023-10-10 22:58:21 +0800

    Commit

--- DSA_Programs/queues-adt/circular-array-implementation/main.c
+++ DSA_Programs/queues-adt/circular-array-implementation/main.c
@@ -11,6 +11,7 @@ typedef struct {
 void initialize(Queue* Q);
 void enqueue(Queue* Q, int elem);
 void dequeue(Queue* Q);
+void display(Queue Q);
 
 int main()
 {
@@ -22,30 +23,49 @@ int main()
     enqueue(&chain, 23);
     enqueue(&chain, 41);
 
+    display(chain);
+
     return 0;
 }
 
 void initialize(Queue* Q)
 {
-    Q->front = -1;
-    Q->rear = -1;
+    Q->front = 1;
+    Q->rear = 0;
+}
+
+int isFull(Queue Q)
+{
+    return (Q.front + 2) % MAX != Q.rear;
+}
+
+int isEmpty(Queue Q)
+{
+    return (Q.front + 1) % MAX != Q.rear;
 }
 
 void enqueue(Queue* Q, int elem)
 {
-    if((Q->rear + 1) % MAX != Q->front) {
-        Q->arr[++Q->rear] = elem;
-        if((Q->front+1) % MAX == Q->rear) {
-            Q->front++;
-        }
+    if(!isFull(*Q)) {
+        Q->rear = (Q->rear+1) % MAX;
+        Q->arr[Q->rear] = elem;
     }
 }
 
 void dequeue(Queue* Q)
 {
-    if(Q->front != Q->rear) {
-        Q->front++;
+    if(!isEmpty(*Q)) {
+        Q->front = (Q->front + 1) % MAX;
     }
 }
 
+
+void display(Queue Q)
+{
+    int x;
+
+    for(x = 0; x <= Q.rear; x++) {
+        printf("ELEMENT AT INDEX %02d : %d\n", x, Q.arr[x]);
+    }
+}
 
